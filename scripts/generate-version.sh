902320487ae109e98daa0b349d5378edf8651665
michavdijk <michavdijk@gmail.com>
2026-05-09 11:33:34 +0200

    Laatste deel versienummer vullen met tijdstip.

--- scripts/generate-version.sh
+++ scripts/generate-version.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 timezone="${VERSION_TIMEZONE:-Europe/Amsterdam}"
-run_number="${VERSION_RUN_NUMBER:-${GITHUB_RUN_NUMBER:-0}}"
 version_date="$(TZ="$timezone" date '+%Y.%m.%d')"
+version_time="$(TZ="$timezone" date '+%H%M')"
 build_date="$(TZ="$timezone" date '+%Y-%m-%dT%H:%M:%S%z')"
 commit="${VERSION_COMMIT:-${GITHUB_SHA:-}}"
 
@@ -14,4 +14,4 @@ fi
 commit="${commit:-unknown}"
 
 printf '{\n  "version": "%s.%s",\n  "date": "%s",\n  "commit": "%s"\n}\n' \
-  "$version_date" "$run_number" "$build_date" "$commit" > version.json
+  "$version_date" "$version_time" "$build_date" "$commit" > version.json
